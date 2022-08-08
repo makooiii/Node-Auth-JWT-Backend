59ded3769e14e8558ab801c166f1d1636297f462
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2022-08-08 21:21:59 +0800

    secondary commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Node Auth JWT Backend
- 
+
+# when starting
+
+# npm init / npm install express / npm install nodemon / npm install mongoose / npm install @hapi/joi
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// import routes
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+// connect to db
+mongoose.connect(process.env.DB_CONNECT, () => {
+  console.log("Connected to db");
+});
+
+// Middlewares
+app.use(express.json());
+
+// Route Middlewares
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => {
+  console.log("Server up");
+});
